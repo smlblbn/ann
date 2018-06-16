a49a1c8883bb65c354d970519c0841ac2313be69
ismail balaban <smlblbn@gmail.com>
2018-06-16 13:39:53 +0300

    Add files via upload
    initial commit

---
+++ ann.py
@@ -0,0 +1,183 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from layers import *
+
+class ANN(object):
+	"""
+    This class implements a modular fully-connected neural network with an 
+    arbitrary number of hidden layers, ReLU nonlinearities, and a L2 loss
+    function. For a network with L layers, the architecture will be
+
+    {affine - relu} x (L - 1) - affine - loss
+
+    where the {...} block is repeated L - 1 times.
+	"""
+	
+	def __init__(self, hidden_dims, input_dim, weight_scale=1e-2, dtype=np.float32):
+  
+		self.num_layers = 1 + len(hidden_dims)
+		self.dtype = dtype
+		self.params = {}
+
+
+		"""
+		In this part of the code, all parameters of the network should be
+		initialized and stored in the self.params dictionary. For a layer "l", you
+		should store the weights as "Wl" and biases as "bl". For example, for the 
+		first layer, "W0" and "b0" should be initialized and stored. These
+		parameters will be used in training and prediction phases. Weights should
+		be initialized with random numbers frome a normal distribution having zero
+		mean and standard deviation equal to weight_scale argument. Biases should
+		be initialized to zero.
+
+		Inputs:
+		hidden_dims: dimensions of hidden layers, a list object.
+		input_dim: dimension of data vectors
+		weight_scale: scale of initial random weights
+		dtype: type of the parameters
+		"""
+
+		############################################################################
+		#                            START OF YOUR CODE                            #
+		############################################################################
+		qwe = [input_dim]+hidden_dims+[1]
+		for i, neuron in enumerate(qwe[:-1]):
+			self.params['W'+str(i)] = np.random.normal(0, weight_scale, (qwe[i], qwe[i+1]))
+			self.params['b'+str(i)] = np.zeros(qwe[i+1])
+		############################################################################
+		#                             END OF YOUR CODE                             #
+		############################################################################
+
+		# Cast all parameters to the correct datatype
+		for k, v in self.params.iteritems():
+			self.params[k] = v.astype(dtype)
+
+
+	def loss(self, X, y=None):
+		"""
+		This function is used for computing the loss and gradient for the network.
+
+		Inputs:
+		X: input data, an array of shape (N, d)
+		y: labels, an array of shape (N,). y[i] is the label of datum X[i].
+
+		Outputs:
+		If y is None, then run a test-time forward pass of the model and return:
+		- predictions: an array of shape (N,) giving target predictions, where
+		  preds[i] is the regression output for X[i].
+		Else return:
+		- loss: data loss computed using L2_loss
+		- grads: a dictionary of gradients where the keys are the parameters of
+		  the network.
+		"""
+		X = X.astype(self.dtype)
+		mode = 'test' if y is None else 'train'
+
+		preds = None
+
+		"""
+		TODO: Implement the forward pass for the network using forward functions
+		implemented in layers.py. Compute the predictions of the target variable
+		for each datum and store them in the preds variable. Hint: You can use a
+		dictionary like self.params to store intermediate results.
+		"""
+		############################################################################
+		#                            START OF YOUR CODE                            #
+		############################################################################
+		if self.num_layers == 1:
+			preds, self.params['cache_a0'] = affine_forward(X, self.params['W0'], self.params['b0'])
+		else:
+			out, self.params['cache_a0'] = affine_forward(X, self.params['W0'], self.params['b0'])
+			out, self.params['cache_r0'] = relu_forward(out)
+			for l in range(self.num_layers-2):
+				out, self.params['cache_a'+str(l+1)] = affine_forward(out, self.params['W'+str(l+1)], self.params['b'+str(l+1)])
+				out, self.params['cache_r'+str(l+1)] = relu_forward(out)
+
+			preds, self.params['cache_a'+str(self.num_layers-1)] = affine_forward(out, self.params['W'+str(self.num_layers-1)], self.params['b'+str(self.num_layers-1)])
+		############################################################################
+		#                             END OF YOUR CODE                             #
+		############################################################################
+
+		# If test mode return early
+		if mode == 'test':
+			return preds
+
+		loss, grads = 0.0, {}
+
+		"""
+		TODO: Implement the backward pass for the network using backward functions
+		implemented in layers.py. Compute data loss using L2_loss and gradients
+		for the parameters. Store the gradient for parameter self.params[p] in
+		grads[p].
+		"""
+		############################################################################
+		#                            START OF YOUR CODE                            #
+		############################################################################
+		loss, dx = L2_loss(preds, np.reshape(y, (-1,1)))
+
+		if self.num_layers == 1:
+			grads['dx_a0'], grads['dw_a0'], grads['db_a0'] = affine_backward(dx, self.params['cache_a0'])
+		else:
+			grads['dx_a'+str(self.num_layers-1)], grads['dw_a'+str(self.num_layers-1)], grads['db_a'+str(self.num_layers-1)] =  affine_backward(dx, self.params['cache_a'+str(self.num_layers-1)])
+			for l in reversed(range(0,self.num_layers-1)):
+				dx = relu_backward(grads['dx_a'+str(l+1)], self.params['cache_r'+str(l)])
+				grads['dx_a'+str(l)], grads['dw_a'+str(l)], grads['db_a'+str(l)] = affine_backward(dx, self.params['cache_a'+str(l)])
+		############################################################################
+		#                             END OF YOUR CODE                             #
+		############################################################################
+
+		return loss, grads
+
+	def train_validate	(self, X_t, y_t, X_v, y_v, maxEpochs=100, learning_rate=1e-4):
+		"""
+		Train the network using gradient descent algorithm.
+
+		Inputs:
+		X_t: training data, an array of shape (N, d)
+		y_t: training labels, an array of shape (N,). y_t[i] is the label of datum X_t[i].
+		X_v: validation data, an array of shape (M, d)
+		y_v: validation labels, an array of shape (M,). y_v[i] is the label of datum X_v[i].
+		maxEpochs: maximum number of epochs that will be spent for training if not
+			       stopped early.
+		learning_rate: hyperparameter used in traditional gradient descent algorithm
+
+		Outputs:
+		loss_train: Loss history for training set containing loss for each epoch
+		loss_valid: Loss history for validation set containing loss for each epoch
+		"""
+		loss_train = []
+		loss_valid = []
+		############################################################################
+		#                            START OF YOUR CODE                            #
+		############################################################################
+		for i in range(maxEpochs):
+			loss_t, grads = self.loss(X_t, y_t)
+			loss_train.append(loss_t)
+
+			loss_v, grads = self.loss(X_v, y_v)
+			loss_valid.append(loss_v)
+
+			for k, v in grads.iteritems():
+				if k[3] == 'a':
+					if k[1] == 'w':
+						self.params['W'+str(k[4:])] = self.params['W'+str(k[4:])] - learning_rate * v
+					if k[1] == 'b':
+						self.params['b'+str(k[4:])] = self.params['b'+str(k[4:])] - learning_rate * v
+		############################################################################
+		#                             END OF YOUR CODE                             #
+		############################################################################
+		return loss_train, loss_valid
+
+	def predict(self, X):
+		"""
+		Predict the target variable using the trained network.
+
+		Inputs:
+		X: test data, an array of shape (N, d)
+
+		Outputs:
+		preds: an array of shape (N,) giving target predictions, where
+		       preds[i] is the regression output for X[i].
+		"""
+		return self.loss(X,None)
---
+++ driver.py
@@ -0,0 +1,141 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time as time
+
+from layers import *
+from test import *
+from ann import *
+from data import *
+
+np.random.seed(499)
+
+print('Simple method Tests\n')
+test_affine_forward()
+test_affine_backward()
+test_relu_forward()
+test_relu_backward()
+test_L2_loss()
+test_ANN_predict()
+
+'''
+# If you want to see all result you can comment out this part and comment next part
+node_num = 30
+input_num = 2
+num_split = 5
+epoch = 500
+
+hidden_layer_number = [[node_num], [node_num,node_num], [node_num, node_num, node_num]]
+data_set_name = ['./set1.dat', './set2.dat']
+learning_rates = [1e-4, 5e-4, 1e-3]
+'''
+
+# Parameters are intilized for 3 hidden layer and 1 learning rate and node number 30 epoch 500
+node_num = 30
+input_num = 2
+num_split = 5
+epoch = 500
+
+hidden_layer_number = [[node_num, node_num, node_num]]
+data_set_name = ['./set1.dat', './set2.dat']
+learning_rates = [5e-4]
+
+all_net = []
+_loss_train = []
+_loss_valid = []
+
+begin_test = time.time()
+print('\nBegin test\n')
+
+for i, name in enumerate(data_set_name):
+
+	# read data sets
+	begin_dataset = time.time()
+	print('Begin train and test for ' + data_set_name[i][2:6] + '\n')
+
+	X_train, Y_train, X_test, Y_test = read_data(name)
+
+	for j, hidden in enumerate(hidden_layer_number):
+
+		# assign hidden layer numbers
+		begin_layers = time.time()
+		print('Begin train and test for ' + str(len(hidden)) + ' hidden layer\n')
+
+		loss_train_mean = np.ndarray(shape=(len(learning_rates), epoch), dtype=float)
+		loss_valid_mean = np.ndarray(shape=(len(learning_rates), epoch), dtype=float)
+		final_loss = np.ndarray(shape=(len(learning_rates)), dtype=float)
+
+		for k, rate in enumerate(learning_rates):
+
+			# cross validation part for all learning rate exists in list
+			begin_rate = time.time()
+			print('Begin train and validate for rate ' + str(rate) + '\n')
+
+			_loss_train = []
+			_loss_valid = []
+
+			for l in range(num_split):
+
+				net = ANN(hidden, input_num);
+
+				# Split the all train data into validation and train part for cross validation
+				validation_data = X_train[X_train.shape[0]/num_split*l : X_train.shape[0]/num_split*(l+1)]
+				validation_label = Y_train[Y_train.shape[0]/num_split*l : Y_train.shape[0]/num_split*(l+1)]
+
+				train_data = np.concatenate((X_train[0 : X_train.shape[0]/num_split*l], X_train[X_train.shape[0]/num_split*(l+1) : X_train.shape[0]]), axis=0)
+				train_label = np.concatenate((Y_train[0 : Y_train.shape[0]/num_split*l], Y_train[Y_train.shape[0]/num_split*(l+1) : Y_train.shape[0]]), axis=0)
+
+				# calculates the train and validate value for all splits
+				loss_train, loss_valid = net.train_validate(train_data, train_label, validation_data, validation_label, epoch , rate)
+
+				_loss_train.append(loss_train)
+				_loss_valid.append(loss_valid)
+
+			# calculates the mean off 5 splits reult
+			loss_train_mean[k] = np.mean(np.array(_loss_train, dtype=float), axis=0)
+			loss_valid_mean[k] = np.mean(np.array(_loss_valid, dtype=float), axis=0)
+			final_loss[k] = loss_valid_mean[k][-1]
+			print('final loss: ' + str(final_loss[k]))
+
+			end_rate = time.time()
+			print('Finish train and validate for rate ' + str(rate) + ' time: ' + str(end_rate-begin_rate) + ' seconds\n')
+
+			'''
+			# plots the mean value of loss and validation results for all rates
+			plt.plot(np.arange(epoch), loss_train_mean[k], label='train')
+			plt.plot(np.arange(epoch), loss_valid_mean[k], label='validation')
+			plt.title('Loss Function for ' + str(len(hidden)) + ' hidden layer rate ' + str(learning_rates[k]))
+			plt.legend(loc='upper right')
+			plt.show()
+			'''
+
+		# plots the mean value of loss and validation results for best rates
+		plt.plot(np.arange(epoch), loss_train_mean[np.argmin(final_loss)], label='train')
+		plt.plot(np.arange(epoch), loss_valid_mean[np.argmin(final_loss)], label='validation')
+		plt.title('Loss Function for ' + str(len(hidden)) + ' hidden layer rate ' + str(learning_rates[np.argmin(final_loss)]))
+		plt.legend(loc='upper right')
+		plt.show()
+
+		# trains the new ann with all train data
+		_rate = learning_rates[np.argmin(final_loss)]
+		net1 = ANN(hidden, input_num);
+		net1.train_validate(X_train, Y_train, X_test, Y_test, epoch , _rate)
+
+		# predicts the test data result with this model
+		pred_test = net1.predict(X_test)
+		dist_test = np.abs(pred_test - Y_test)
+
+		# prints the min, max and avg reult of distance
+		print('Minimum distance: ' + str(np.min(dist_test)))
+		print('Maximum distance: ' + str(np.max(dist_test)))
+		print('Average distance: ' + str(np.mean(dist_test)))
+		print('Relative error: ' + str( np.max(np.abs(Y_test - pred_test) / (np.maximum(1e-8, np.abs(Y_test) + np.abs(pred_test))))))
+		print('\n')
+
+		end_layers = time.time()
+		print('Finish Train and Test for ' + str(len(hidden)) + ' hidden layer time: ' + str(end_layers-begin_layers) + ' seconds\n')
+
+	end_dataset = time.time()
+	print('Finish train and test for ' + data_set_name[i][2:6] + ' time: ' + str(end_dataset-begin_dataset) + ' seconds\n')
+
+end_test = time.time()
+print('Finish test time: ' + str(end_test-begin_test) + ' seconds\n')
